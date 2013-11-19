3a2ce998c56153cb9f7c1454ef7988775ea1d4c8
David Burhans <david.burhans@gmail.com>
2013-11-18 23:18:16 -0700

    Added server-side code to prototype the Text-to-Braille translation

---
+++ Sense.Server/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Sense.Server.InputProvider;
+using Sense.Server.DisplayProvider;
+
+namespace Sense.Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            BrailleInputProvider.CharacterMap.LoadFromFile("Text2Braille.txt");
+            var input = new BrailleInputProvider();
+            input.Text = "This is a test";
+
+            var displayProvider = new ConsoleDisplayProvider();
+            var display = displayProvider.GetDisplay();
+            var displayContents = input.GetInput();
+            
+            while (displayContents != null)
+            {
+                display.Contents = displayContents;
+                displayProvider.RenderDisplay();
+                displayContents = input.GetInput();
+                Console.ReadLine();
+            }
+        }
+    }
+}
